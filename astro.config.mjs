e58132382784e5f84c1a59f6386eb221256cacf0
Vanderlei Chepanski <vchepanski@outlook.com>
2025-11-01 01:43:49 -0300

    Configure Astro Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({ runtime: 'nodejs' }),
   vite: {
     plugins: [tailwindcss()]
   }
